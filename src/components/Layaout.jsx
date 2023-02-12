7069baf420e87f92e5995abf192d3237c0320a6a
MasterPorti <masterportilol@gmail.com>
2023-02-12 11:56:44 -0800

    add resposive menu

--- src/components/Layaout.jsx
+++ src/components/Layaout.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import { Outlet } from 'react-router-dom'
 import './Layaout.scss'
 import Logo from '../assets/Logo.svg'
+import MobileButton from '../assets/sandwich.svg'
+import CloseButton from '../assets/cross.svg'
 const Layaout = () => {
+  const [hidden, setHidden] = useState(false)
   return (
     <div className='main-container'>
-      <div className='ly-container'>
+      <button className='ly-mobileButton' onClick={() => setHidden(!hidden)}><img src={MobileButton} /></button>
+      <div className={`ly-container ${hidden ? 'ly-hidden' : ''} `}>
+        <button className={hidden ? 'ly-closeButton' : 'ly-closeButtonHidden'} onClick={() => setHidden(!hidden)}><img src={CloseButton} /></button>
         <img src={Logo} alt='Logo' className='ly-logo' />
         <main className='ly-menuContainer'>
           <p className='ly-menuItem'>Inicio</p>
